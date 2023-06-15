99baa2df53afebfc9836f54ec08fee304815343a
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-06-15 22:34:09 +0530

    edit icon

--- src/component/BlogDetail.js
+++ src/component/BlogDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Blogdetail.css"
 import { useLocation, useNavigate } from '../../node_modules/react-router-dom/dist/index';
 import Navbar from './Navbar';
+import {FaEdit  } from 'react-icons/fa';
 
 
 const BlogDetail = () => {
@@ -12,6 +13,9 @@ const handleBack=()=>{
 navigate('/')
 }
 
+const user = localStorage.getItem("bloguser");
+  const userparse = JSON.parse(user);
+  const username = userparse[0].name;
 
   return (
     <>
@@ -19,9 +23,10 @@ navigate('/')
     <div className="blog-post">
     <h1><span className='arrow' onClick={handleBack}>&#8592;</span>{post?.title}</h1>
     <p className="author-info">
-      By <span className="author-name">{post?.authorName}</span> |{' '}
-      <span className="topic">{post?.topic}</span>
-    </p>
+  By <span className="author-name">{post?.authorName}</span> |{' '}
+  <span className="topic">{post?.topic}</span>
+  <span className="edit-icon">{post.authorName === username? <FaEdit /> : ""}</span>
+</p>
     <div className='blog-item' style={{ backgroundImage: `url(${post?.backgroundImage})` }}></div>
     <div dangerouslySetInnerHTML={{ __html: post?.content }} />
   </div>
